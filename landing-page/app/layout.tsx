2963b6ae4afd34df9549e1b88dfb958575147631
kamal eth <kamalthedev7@gmail.com>
2024-08-10 02:43:19 +0000

    ensure that the plugin is working properly

--- landing-page/app/layout.tsx
+++ landing-page/app/layout.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css"
 import { Metadata } from "next"
 
 import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
